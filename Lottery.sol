5d1d0d69f0158efcf87a0739f6659d8ba662d7d7
Linas Spukas <l.spukas@gmail.com>
2018-02-20 21:51:25 +0200

    Add security that only manage could pick winner

--- Lottery.sol
+++ Lottery.sol
@@ -19,6 +19,8 @@ contract Lottery {
     }
     
     function pickWinner() public {
+        require(msg.sender == manager);
+        
         uint index = random() % players.length;
         players[index].transfer(this.balance);
         players = new address[](0);
