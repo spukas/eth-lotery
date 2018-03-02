ac9787f0557a3a00d928945bd0995a5d65d9af50
Linas Spukas <l.spukas@gmail.com>
2018-03-02 08:05:13 +0100

    Use contract methods to render current state of the contract

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,19 @@ import lottery from './lottery';
 class App extends Component {
   state = {
     manager: '',
+    players: '',
+    balance: '0',
   };
 
   async componentDidMount() {
-    const manager = await lottery.methods.manager().call();
+    const [manager, players, balance] = await Promise.all([
+      lottery.methods.manager().call(),
+      lottery.methods.getPlayers().call(),
+      lottery.methods.getBalance().call(),
+      web3.eth.getBalance(lottery.options.address),
+    ]);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {
@@ -19,7 +26,9 @@ class App extends Component {
       <div>
         <h2>Lottery Contract</h2>
 
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>This contract is managed by: {this.state.manager}</p>
+        <p>Currently participating playes: {this.state.players.length}</p>
+        <p>To win: {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
       </div>
     );
   }
