fb1c9a18a3b2b1e7d4625f629c849910192f6aa0
Linas Spukas <l.spukas@gmail.com>
2018-03-03 18:00:47 +0100

    Create form for user to enter the lottery

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     manager: '',
     players: '',
     balance: '0',
+    value: '',
   };
 
   async componentDidMount() {
@@ -21,6 +22,21 @@ class App extends Component {
     this.setState({ manager, players, balance });
   }
 
+  handleInputChange = event => this.setState({ value: event.target.value });
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    console.log({ accounts });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether'),
+    });
+  };
+
   render() {
     return (
       <div>
@@ -29,6 +45,14 @@ class App extends Component {
         <p>This contract is managed by: {this.state.manager}</p>
         <p>Currently participating playes: {this.state.players.length}</p>
         <p>To win: {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
+
+        <hr />
+
+        <form onSubmit={this.handleSubmit}>
+          <label>Amount of ether to enter</label>
+          <input value={this.state.value} onChange={this.handleInputChange} />
+          <button type="submit">Enter</button>
+        </form>
       </div>
     );
   }
